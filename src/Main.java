a0ef299b1bcc75fb300f9a35f715032666866655
Filker Christina <christinafilker23@gmail.com>
2020-07-06 11:13:43 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public Main() {
+    }
+
+    public static void main(String[] args) {
+        long miles = SpeedConverter.toMilesPerHour(10.5);
+        System.out.println("Miles = " + miles);
+
+        SpeedConverter.printConversion(10.5);
+
+
+
+
+    }
+}
---
+++ src/SpeedConverter.java
@@ -0,0 +1,40 @@
+public class SpeedConverter {
+
+    public static void printConversion(double kilometersPerHour)
+    {
+        if(kilometersPerHour < 0)
+        {
+            System.out.println("Invalid Value");
+
+        }
+        else
+        {
+            long milesPerHour = toMilesPerHour(kilometersPerHour);
+            System.out.println(kilometersPerHour +
+                    "km/h= "+ milesPerHour +
+                    "mi/h");
+        }
+
+    }
+
+    public static long toMilesPerHour(double kilometersPerHour)
+    {
+        if(kilometersPerHour < 0)
+        {
+            return -1;
+        }
+
+        return Math.round(kilometersPerHour / 1.609);
+    }
+
+    private int limit;
+    private String name;
+
+    public SpeedConverter(int limit, String name) {
+        this.limit = limit;
+        this.name = name;
+
+
+
+    }
+}
